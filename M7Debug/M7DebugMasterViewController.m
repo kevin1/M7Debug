b967fb330971fbf816ba3e73afa8fad0dd92384a
Kevin Chen <kevinchen2003@gmail.com>
2014-01-02 16:53:51 -0800

    (Untested) Wrote the code in one go. Many rough edges.
    No metadata or assets
    Not tested on actual hardware (simulator doesn’t support fake M7)
    App’s design is kind of boring

--- M7Debug/M7DebugMasterViewController.m
+++ M7Debug/M7DebugMasterViewController.m
@@ -8,14 +8,145 @@
 
 #import "M7DebugMasterViewController.h"
 
-#import "M7DebugDetailViewController.h"
+@interface M7DebugMasterViewController()
 
-@interface M7DebugMasterViewController () {
-    NSMutableArray *_objects;
-}
+@property (strong, nonatomic) NSMutableArray *objects;
+@property (strong, nonatomic) NSTimer *refreshTimer;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@property (strong, nonatomic) NSString *notAvailableSymbol;
+
+@property (strong, nonatomic) CMMotionActivityManager *activityManager;
+@property (strong, nonatomic) NSDateFormatter *dateFormatter;
+
+@property (strong, nonatomic) CMStepCounter *stepCounter;
+
 @end
 
 @implementation M7DebugMasterViewController
+
+- (NSMutableArray *)objects
+{
+	if (!_objects)
+	{
+        _objects = [[NSMutableArray alloc] initWithCapacity:4];
+		
+		[_objects addObject:[self cellWithLabel:@"Activity"]];           // 0
+		[_objects addObject:[self cellWithLabel:@"Confidence"]];         // 1
+		[_objects addObject:[self cellWithLabel:@"Start Time"]];         // 2
+		[_objects addObject:[self cellWithLabel:@"Steps Since Launch"]]; // 3
+    }
+	return _objects;
+}
+
+- (UITableViewCell *)cellWithLabel:(NSString *)label
+{
+	return [self cellWithLabel:label withDetailLabel:self.notAvailableSymbol];
+}
+
+- (UITableViewCell *)cellWithLabel:(NSString *)label withDetailLabel:(NSString *)detailLabel
+{
+	UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"keyLeft"];
+	cell.textLabel.text = label;
+	cell.detailTextLabel.text = detailLabel;
+	
+	return cell;
+}
+
+- (NSString *)notAvailableSymbol
+{
+	if (!_notAvailableSymbol)
+	{
+		_notAvailableSymbol = @"—";
+	}
+	return _notAvailableSymbol;
+}
+
+- (CMMotionActivityManager *)activityManager
+{
+	// Lazy initialization
+	if (!_activityManager && [CMMotionActivityManager isActivityAvailable])
+	{
+		_activityManager = [[CMMotionActivityManager alloc] init];
+		
+		// Block that will handle activity updates. 
+		// Generates strings from CMMotionActivity and applies them to the view.
+		CMMotionActivityHandler activityHandler = ^(CMMotionActivity *activity)
+		{
+			// Current activity
+			NSString *activityName = self.notAvailableSymbol;
+			if (activity.stationary) activityName = @"Stationary";
+			else if (activity.walking) activityName = @"Walking";
+			else if (activity.running) activityName = @"Running";
+			else if (activity.automotive) activityName = @"Automotive";
+			else if (activity.unknown) activityName = @"Unknown";
+			
+			[[[self.objects objectAtIndex:0] detailTextLabel] setText:activityName];
+			
+			// Confidence
+			NSString *confidence = self.notAvailableSymbol;
+			if (activity.confidence == CMMotionActivityConfidenceHigh) confidence = @"High";
+			else if (activity.confidence == CMMotionActivityConfidenceMedium) confidence = @"Medium";
+			else if (activity.confidence == CMMotionActivityConfidenceLow) confidence = @"Low";
+			
+			[[[self.objects objectAtIndex:2] detailTextLabel] setText:confidence];
+			
+			// Start date
+			NSString *startTime = [self.dateFormatter stringFromDate:[activity startDate]];
+			[[[self.objects objectAtIndex:3] detailTextLabel] setText:startTime];
+			
+			[self.tableView reloadData];
+		};
+		
+		[_activityManager startActivityUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:activityHandler];
+	}
+	
+	return _activityManager;
+}
+
+- (CMStepCounter *)stepCounter
+{
+	if (!_stepCounter && [CMStepCounter isStepCountingAvailable])
+	{
+		_stepCounter = [[CMStepCounter alloc] init];
+		
+		CMStepUpdateHandler stepHandler = ^(NSInteger numberOfSteps, NSDate *timestamp, NSError *error)
+		{
+			// Generate NSString representing number of steps or error
+			NSString *stepsText = self.notAvailableSymbol;
+			if (error != nil)
+			{
+				stepsText = @"Error";
+				NSLog(@"CMStepUpdateHandler received error: %@", [error description]); // TODO: replace w/ uialertview?
+			}
+			else
+			{
+				stepsText = [NSString stringWithFormat:@"%ld", numberOfSteps];
+			}
+			
+			// Apply to view
+			[[[self.objects objectAtIndex:4] detailTextLabel] setText:stepsText];
+			[self.tableView reloadData];
+		};
+		
+		// Attach the step handler block to the step manager. Update every time there is a new step.
+		[_stepCounter startStepCountingUpdatesToQueue:[NSOperationQueue mainQueue] updateOn:1 withHandler:stepHandler];
+	}
+	
+	return _stepCounter;
+}
+
+- (NSDateFormatter *)dateFormatter
+{
+	if (!_dateFormatter)
+	{
+		_dateFormatter = [[NSDateFormatter alloc] init];
+		
+		[_dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+		[_dateFormatter setDateStyle:NSDateFormatterShortStyle];
+	}
+	return _dateFormatter;
+}
 
 - (void)awakeFromNib
 {
@@ -26,26 +157,27 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-	self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-	UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-	self.navigationItem.rightBarButtonItem = addButton;
+	self.navigationItem.leftBarButtonItem = nil;
+	self.navigationItem.rightBarButtonItem = nil;
 }
+/* timer no longer needed b/c we updating happens in callbacks from CoreMotion
+- (void)viewWillAppear:(BOOL)animated
+{
+	// start the timer
+	self.refreshTimer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(updateMotion) userInfo:nil repeats:YES];
+	[self.refreshTimer setTolerance:0.05];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	// stop the timer
+	[_refreshTimer invalidate];
+}*/
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)insertNewObject:(id)sender
-{
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
-    }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
 
 #pragma mark - Table View
@@ -57,38 +189,40 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-	return _objects.count;
+	return [self.objects count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-
-	NSDate *object = _objects[indexPath.row];
-	cell.textLabel.text = [object description];
-    return cell;
+    //UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+	// NSIndexPath works sort of like a tree. This gets the second layer. (index starts from 0)
+	return [self.objects objectAtIndex:[indexPath indexAtPosition:1]];
 }
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+    // No cells should be editable
+    return NO;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
+    /*if (editingStyle == UITableViewCellEditingStyleDelete)
+	{
         [_objects removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+    } 
+	else if (editingStyle == UITableViewCellEditingStyleInsert)
+	{
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
+    }*/
 }
 
 /*
 // Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
 {
+	
 }
 */
 
@@ -103,10 +237,12 @@
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    if ([[segue identifier] isEqualToString:@"showDetail"]) {
+    if ([[segue identifier] isEqualToString:@"showDetail"])
+	{
+		/*  this is a single view app
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
         NSDate *object = _objects[indexPath.row];
-        [[segue destinationViewController] setDetailItem:object];
+        [[segue destinationViewController] setDetailItem:object];*/
     }
 }
 
